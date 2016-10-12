062f93582064046f4aa687d07f8a30dd9fb68221
pulse <antynomia@gmail.com>
2016-10-12 18:05:57 +0200

    new file:   loggingru/hooks/poststart.sh

---
+++ loggingru/hooks/poststart.sh
@@ -0,0 +1,18 @@
+###poststart
+sleep 60
+apk add --update curl bash jq
+echo "Starting poststart hook"
+
+
+
+  curl -X PUT  -d '{
+    "transient": {
+      "cluster.routing.allocation.enable": "all"
+    }
+  }' "http://$ELASTICSEARCH_HOST:9200/_cluster/settings" --connect-timeout 1 -m 2
+
+
+bash /hooks/reallocate.sh &
+
+exit 0
+##
