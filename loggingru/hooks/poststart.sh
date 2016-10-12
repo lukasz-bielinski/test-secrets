d0a79da8efcd71f900b2b62ab554927bf23cc439
pulse <antynomia@gmail.com>
2016-10-12 18:50:21 +0200

    modified:   loggingru/hooks/poststart.sh

--- loggingru/hooks/poststart.sh
+++ loggingru/hooks/poststart.sh
@@ -12,7 +12,7 @@ echo "Starting poststart hook"
   }' "http://$ELASTICSEARCH_HOST:9200/_cluster/settings" --connect-timeout 1 -m 2
 
 
-bash /hooks/reallocate.sh &
+# bash /hooks/reallocate.sh &
 
 exit 0
 #
